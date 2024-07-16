19c6919e71a3761192b352628c8412edd15fd7f8
Oleg Semashko <semashko.olezhka@inbox.ru>
2024-07-16 23:43:11 +0300

    init commit

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -18,66 +18,90 @@ func createAccount(db *sql.DB) (*models.Account, error) {
 	return &models.Account{ID: id, DB: db}, nil
 }
 
-func createAccountHandler(c echo.Context, db *sql.DB) error {
-	account, err := createAccount(db)
-	if err != nil {
-		return err
-	}
-	return c.JSON(http.StatusOK, account)
-}
-
-func depositHandler(db *sql.DB, c echo.Context) error {
-	accountID := c.Param("id")
-	amount, err := strconv.ParseFloat(c.QueryParam("amount"), 64)
-	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, "Invalid amount")
-	}
-
-	resultChan := make(chan error)
-	go func() {
-		defer close(resultChan)
-		account := &models.Account{ID: accountID, DB: db}
-		err := account.Deposit(amount)
+func CreateAccountHandler(db *sql.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		account, err := createAccount(db)
 		if err != nil {
-			resultChan <- err
-		} else {
-			resultChan <- nil
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 		}
-	}()
-
-	if err := <-resultChan; err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		return c.JSON(http.StatusOK, account)
 	}
-
-	return c.JSON(http.StatusOK, map[string]string{"status": "deposit completed"})
 }
 
-func withdrawHandler(db *sql.DB, c echo.Context) error {
-	accountID := c.Param("id")
-	amount, err := strconv.ParseFloat(c.QueryParam("amount"), 64)
-	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, "Invalid amount")
-	}
-
-	resultChan := make(chan error)
-	go func() {
-		defer close(resultChan)
-		account := &models.Account{ID: accountID, DB: db}
-		err := account.Withdraw(amount)
+func DepositHandler(db *sql.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		accountID := c.Param("id")
+		amount, err := strconv.ParseFloat(c.QueryParam("amount"), 64)
 		if err != nil {
-			resultChan <- err
-		} else {
-			resultChan <- nil
+			return echo.NewHTTPError(http.StatusBadRequest, "Invalid amount")
 		}
-	}()
 
-	if err := <-resultChan; err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		resultChan := make(chan error)
+		go func() {
+			defer close(resultChan)
+			account := &models.Account{ID: accountID, DB: db}
+			err := account.Deposit(amount)
+			if err != nil {
+				resultChan <- err
+			} else {
+				resultChan <- nil
+			}
+		}()
+
+		if err := <-resultChan; err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
+
+		return c.JSON(http.StatusOK, map[string]string{"status": "deposit completed"})
 	}
-
-	return c.JSON(http.StatusOK, map[string]string{"status": "withdrawal completed"})
 }
 
-func balanceHandler(db *sql.DB, c echo.Context) error {
+func WithdrawHandler(db *sql.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		accountID := c.Param("id")
+		amountStr := c.QueryParam("amount")
+		amount, err := strconv.ParseFloat(amountStr, 64)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, "Invalid amount")
+		}
 
+		resultChan := make(chan error)
+		go func() {
+			defer close(resultChan)
+			account := &models.Account{ID: accountID, DB: db}
+			err := account.Withdraw(amount)
+			if err != nil {
+				resultChan <- err
+			} else {
+				resultChan <- nil
+			}
+		}()
+
+		if err := <-resultChan; err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
+
+		return c.JSON(http.StatusOK, map[string]string{"status": "withdrawal completed"})
+	}
+}
+
+func BalanceHandler(db *sql.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		accountID := c.Param("id")
+
+		resultChan := make(chan *models.BalanceResult)
+		go func() {
+			defer close(resultChan)
+			account := &models.Account{ID: accountID, DB: db}
+			balance, err := account.GetBalance()
+			resultChan <- &models.BalanceResult{Balance: balance, Err: err}
+		}()
+
+		result := <-resultChan
+		if result.Err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, result.Err.Error())
+		}
+
+		return c.JSON(http.StatusOK, map[string]float64{"balance": result.Balance})
+	}
 }
